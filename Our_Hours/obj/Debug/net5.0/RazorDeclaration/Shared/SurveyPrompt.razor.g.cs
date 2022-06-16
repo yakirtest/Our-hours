d3b19ee7efdce359793b3806b7aa0088743e7aa2
IsaacPol <itzik.poltorak25@gmail.com>
2022-06-16 11:47:48 +0300

    organize some folder

--- Our_Hours/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ Our_Hours/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace Our_Hours.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 1 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 2 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 3 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 4 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 5 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 6 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 7 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 8 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 9 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Our_Hours;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\GitHub\Our_Hours\Our_Hours\_Imports.razor"
+#line 10 "C:\GitHub\Our-hours\Our_Hours\_Imports.razor"
 using Our_Hours.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Our_Hours.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\GitHub\Our_Hours\Our_Hours\Shared\SurveyPrompt.razor"
+#line 12 "C:\GitHub\Our-hours\Our_Hours\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
